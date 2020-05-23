beba014b0d053dccd95c5b2fa51fd110d60539ea
Kolawole <olaniyi.jibola152@gmail.com>
2020-05-23 22:21:37 +0100

    make it better

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const bodyParser = require('body-parser')
 const handler = require('./handlers/controller.js')
 const cors = require('cors')
 
-var allowCrossDomain = function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+// var allowCrossDomain = function(req, res, next){
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    next()
-}
+//     next()
+// }
 
 const app = express()
-app.use(allowCrossDomain)
+//app.use(allowCrossDomain)
 var port = process.env.PORT || 3001
 
 
